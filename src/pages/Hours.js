c0d55e94919d3d97e73bd965d07c082933634406
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-10-09 19:35:31 -0600

    finish hours of operation page

--- src/pages/Hours.js
+++ src/pages/Hours.js
@@ -1,6 +1,32 @@
+import "./Hours.css"
+
 function Hours() {
   return (
-    <h1>Hours</h1>
+    <div className="business-hours">
+    <h1>HOURS OF OPERATION</h1>
+    <div className="hours">
+      <div className="day">Sunday</div>
+      <div className="hours-open">10AM - 8PM</div>
+      
+      <div className="day">Monday</div>
+      <div className="hours-open">10AM - 8PM</div>
+      
+      <div className="day">Tuesday</div>
+      <div className="hours-open">10AM - 8PM</div>
+      
+      <div className="day">Wednesday</div>
+      <div className="hours-open">10AM - 8PM</div>
+      
+      <div className="day">Thursday</div>
+      <div className="hours-open">10AM - 8PM</div>
+      
+      <div className="day">Friday</div>
+      <div className="hours-open">10AM - 8PM</div>
+      
+      <div className="day">Saturday</div>
+      <div className="hours-open">10AM - 6PM</div>
+    </div>
+  </div>
   );
 }
 
