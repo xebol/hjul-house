a880b10e731f9fc6f6db45ffaa3c56bb2d13a234
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-10-10 13:07:34 -0600

    start contact us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,15 +4,13 @@ function About() {
   return (
     <div className="about">
       <div className="about-header">
-        <h1>About Hyul House</h1>
+        <h1>About Hjul House</h1>
       </div>
       <div className="about-us">
         <p>
           Welcome to your trusted destination for hassle-free bike repairs and maintenance.</p>
 
-          <p>At Hyul House, we are passionate about cycling and committed to keeping your wheels turning smoothly. We understand the importance of a well-maintained bike, whether it's for your daily commute, leisurely rides, or competitive racing.
-
-          Our team of experienced and certified bike mechanics is dedicated to providing top-notch service, ensuring that your bike is in the best possible condition. With our expertise and attention to detail, you can trust us to handle everything from minor adjustments to major overhauls.
+          <p>At Hjul House, we are passionate about cycling and committed to keeping your wheels turning smoothly. We understand the importance of a well-maintained bike, whether it's for your daily commute, leisurely rides, or competitive racing.
         </p>
       </div>
     </div>
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,6 +1,24 @@
+import contact from "../images/contact-us.jpg";
+import "./ContactUs.css";
+
+
 function ContactUs() {
   return (
-    <h1>Contact Us</h1>
+    <div className="contact-us">
+      <img src={contact} alt="contact" className="contact-us-img"></img>
+      <div className="contact-info">
+        <h1>Contact Us</h1>
+        <form className="contact-form">
+          <label> Fullname</label>
+          <input type="text" name="name" />
+          <label> Email </label>
+          <input type="email" name="email" />
+          <label>Message</label>
+          <textarea name="message"></textarea>
+          <input type="submit" className="contact-us-btn" />
+        </form>
+      </div>
+    </div>
   );
 }
 
