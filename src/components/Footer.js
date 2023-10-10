4360ebc99c9c3748ed7bfbb49e35a96e92744176
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-10-09 18:54:00 -0600

    install font awesome and finish first draft of footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSquareFacebook, faSquareInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import './Footer.css';
+
 function Footer() {
   return (
-    <h1>Footer</h1>
+    <div className="footer">
+      <div className="socials-container">
+        <h3>Hjul House</h3>
+        <div className="socials">
+          <FontAwesomeIcon icon={faSquareInstagram} className="fa-icon fa-2x" />
+          <FontAwesomeIcon icon={faTwitter} className="fa-icon fa-2x" />
+          <FontAwesomeIcon icon={faSquareFacebook} className="fa-icon fa-2x" />
+        </div>
+      </div>
+      <p>&copy; Hjul House 2023</p>
+    </div>
   );
 }
-
-
 
 export default Footer;
