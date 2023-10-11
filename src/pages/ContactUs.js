23787499751c5084ece91594ba6457d161188610
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-10-10 18:38:18 -0600

    finish constructing contact us page

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,20 +1,47 @@
+import { useState } from "react";
 import contact from "../images/contact-us.jpg";
 import "./ContactUs.css";
 
-
 function ContactUs() {
+  const [info, setInfo] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setInfo({
+      ...info,
+      [name]: value
+    });
+
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setInfo({
+      name: "",
+      email: "",
+      message: ""
+    });
+  };
+
+
   return (
     <div className="contact-us">
       <img src={contact} alt="contact" className="contact-us-img"></img>
       <div className="contact-info">
         <h1>Contact Us</h1>
-        <form className="contact-form">
+        <form autoComplete="off" className="contact-form" onSubmit={handleSubmit}>
           <label> Fullname</label>
-          <input type="text" name="name" />
+          <input type="text" name="name" value={info.name} onChange={handleChange} className="value-input"/>
           <label> Email </label>
-          <input type="email" name="email" />
+          <input type="email" name="email" value={info.email} onChange={handleChange}  className="value-input"/>
           <label>Message</label>
-          <textarea name="message"></textarea>
+          <textarea name="message" className="message-to-send" value={info.message} onChange={handleChange}></textarea>
           <input type="submit" className="contact-us-btn" />
         </form>
       </div>
