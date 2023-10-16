7b1ba5858b44959621168fb92b9ccd59322c32ab
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-10-16 12:54:40 -0600

    add font style and background color to about page and home page

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,11 +7,9 @@ function About() {
         <h1>About Hjul House</h1>
       </div>
       <div className="about-us">
-        <p>
-          Welcome to your trusted destination for hassle-free bike repairs and maintenance.</p>
+        <p>Welcome to your trusted destination for hassle-free bike repairs and maintenance.</p>
 
-          <p>At Hjul House, we are passionate about cycling and committed to keeping your wheels turning smoothly. We understand the importance of a well-maintained bike, whether it's for your daily commute, leisurely rides, or competitive racing.
-        </p>
+          <p>At Hjul House, we are passionate about cycling and committed to keeping your wheels turning smoothly. We understand the importance of a well-maintained bike, whether it's for your daily commute, leisurely rides, or competitive racing.</p>
       </div>
     </div>
   );
