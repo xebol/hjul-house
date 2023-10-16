0a58392eef9060bfc664d3f2a26481da1373aaa9
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-10-16 15:17:48 -0600

    add images and style Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,23 @@
 import "./Navbar.css";
 import { Link, NavLink } from "react-router-dom";
-import logo from "../images/Screenshot 2023-10-09 at 4.35.24 PM.png";
+import bike from "../images/Screenshot 2023-10-09 at 4.35-PhotoRoom.png";
+// import bike from "../images/bike.png";
+
 function Navbar() {
   return (
     <div className="nav">
       <div className="nav-links-container">
-        <div className="nav-links">
-          <Link to="/" as={NavLink} className="link">Home</Link>
-          <Link to="/about" as={NavLink} className="link">About</Link>
-          <Link to="/services" as={NavLink} className="link">Services</Link>
-          <Link to="/contact" as={NavLink} className="link">Contact Us</Link>
-          <Link to="/hours" as={NavLink} className="link">Hours</Link>
-        </div>
-        <div className="logo">
+        <div className="home-nav">
           <Link to="/" as={NavLink} className="link">
-            <img src={logo} alt="logo" className="logo-image" />
+            <img src={bike} alt="bike" className="logo-image" />
           </Link>
         </div>
+      </div>
+      <div className="links-container">
+        <Link to="/about" as={NavLink} className="link">About</Link>
+        <Link to="/services" as={NavLink} className="link">Services</Link>
+        <Link to="/contact" as={NavLink} className="link">Contact Us</Link>
+        <Link to="/hours" as={NavLink} className="link">Hours</Link>
       </div>
     </div>
   );
