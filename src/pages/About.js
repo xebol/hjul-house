3fa3fb333a245089627a8c6b82f840706b60e83a
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-10-17 18:35:44 -0600

    start README and add mission and vision statements

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,17 @@ function About() {
       <div className="about-us">
         <p>Welcome to your trusted destination for hassle-free bike repairs and maintenance.</p>
 
-          <p>At Hjul House, we are passionate about cycling and committed to keeping your wheels turning smoothly. We understand the importance of a well-maintained bike, whether it's for your daily commute, leisurely rides, or competitive racing.</p>
+        <p>At Hjul House, we are passionate about cycling and committed to keeping your wheels turning smoothly. We understand the importance of a well-maintained bike, whether it's for your daily commute, leisurely rides, or competitive racing.</p>
+        
+        <div className="our-mission">
+          <h2>Our Mission</h2>
+          <p>At Hjul House, our mission is to be the trusted companion on your cycling journey. We are dedicated to providing exceptional bicycle repair and maintenance resources to enthusiasts and riders of all levels.</p>
+        </div>
+        <div className="our-vision">
+          <h2>Our Vision</h2>
+          <p>Our vision at Hjul House is to be the go-to resource for all things bicycle repair and service. We envision a future where cycling is accessible to all, made possible through quality maintenance and the shared joy of riding.
+          </p>
+        </div>
       </div>
     </div>
   );
