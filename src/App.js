b35a62455e5d2a855a54a90c71bdb4df8984b272
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-12-05 07:49:01 -0700

    fix redirecting issues

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { HashRouter as Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
