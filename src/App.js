23cea2920b9663d9a8c3e3824a193a46106fbbf4
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-12-05 07:52:01 -0700

    remove new changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import About from "./pages/About";
